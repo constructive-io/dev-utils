eeaa1e1d368d5c67bb1d4ac01f507b5358a9c5fe
Dan Lynch <pyramation@gmail.com>
2025-11-22 19:13:39 -0800

    updates

--- packages/strfy-js/types/json.d.ts
+++
@@ -1,4 +0,0 @@
-import { JSStringifyOptions } from "./js";
-export interface JSONStringifyOptions extends Omit<JSStringifyOptions, 'json'> {
-}
-export declare function jsonStringify(obj: any, options?: JSONStringifyOptions): string;
