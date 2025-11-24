3e0d1b1933044b110c2afa0e85e27926310035bc
Dan Lynch <pyramation@gmail.com>
2025-11-23 16:40:22 -0800

    remove makage due to build issues

--- packages/makage/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { runCopy } from './commands/copy';
-export { runClean } from './commands/clean';
-export { runReadmeFooter } from './commands/readmeFooter';
-export { runAssets } from './commands/assets';
-export { runBuildTs } from './commands/buildTs';
-export { runUpdateWorkspace } from './commands/updateWorkspace';
